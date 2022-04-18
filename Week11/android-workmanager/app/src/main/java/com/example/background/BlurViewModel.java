664bfca8f0f149c56e36f528a0a541a891eb79a3
salmazizah <salma.azizah.tif20@polban.ac.id>
2022-04-18 10:49:08 +0700

    Add Tugas Praktek Week 11

---
+++ Week11/android-workmanager/app/src/main/java/com/example/background/BlurViewModel.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.background;
+
+import static com.example.background.Constants.IMAGE_MANIPULATION_WORK_NAME;
+import static com.example.background.Constants.KEY_IMAGE_URI;
+import static com.example.background.Constants.TAG_OUTPUT;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkContinuation;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import android.app.Application;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.example.background.workers.BlurWorker;
+import com.example.background.workers.CleanupWorker;
+import com.example.background.workers.SaveImageToFileWorker;
+
+import java.util.List;
+
+public class BlurViewModel extends ViewModel {
+    private WorkManager mWorkManager;
+    private Uri mImageUri;
+    private LiveData<List<WorkInfo>> mSavedWorkInfo;
+    private Uri mOutputUri;
+
+    public BlurViewModel(@NonNull Application application) {
+        super();
+        mWorkManager = WorkManager.getInstance(application);
+        mImageUri = getImageUri(application.getApplicationContext());
+        mSavedWorkInfo = mWorkManager.getWorkInfosByTagLiveData(TAG_OUTPUT);
+
+        OneTimeWorkRequest save = new OneTimeWorkRequest.Builder(SaveImageToFileWorker.class)
+                .addTag(TAG_OUTPUT) // This adds the tag
+                .build();
+    }
+
+    /**
+     * Create the WorkRequest to apply the blur and save the resulting image
+     * @param blurLevel The amount to blur the image
+     */
+    void applyBlur(int blurLevel) {
+        // Add WorkRequest to Cleanup temporary images
+        WorkContinuation continuation = mWorkManager
+                .beginUniqueWork(IMAGE_MANIPULATION_WORK_NAME,
+                        ExistingWorkPolicy.REPLACE, OneTimeWorkRequest.from(CleanupWorker.class));
+        // Add WorkRequests to blur the image the number of times requested
+        for (int i = 0; i < blurLevel; i++) {
+            OneTimeWorkRequest.Builder blurBuilder =
+                    new OneTimeWorkRequest.Builder(BlurWorker.class);
+
+            // Input the Uri if this is the first blur operation
+            // After the first blur operation the input will be the output of previous
+            // blur operations.
+            if ( i == 0 ) {
+                blurBuilder.setInputData(createInputDataForUri());
+            }
+
+            continuation = continuation.then(blurBuilder.build());
+        }
+
+        // Create charging constraint
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .build();
+
+        // Add WorkRequest to save the image to the filesystem
+        OneTimeWorkRequest save = new OneTimeWorkRequest.Builder(SaveImageToFileWorker.class)
+                .setConstraints(constraints) // This adds the Constraints
+                .addTag(TAG_OUTPUT)
+                .build();
+
+        continuation = continuation.then(save);
+
+        // Actually start the work
+        continuation.enqueue();
+    }
+
+    private Uri uriOrNull(String uriString) {
+        if (!TextUtils.isEmpty(uriString)) {
+            return Uri.parse(uriString);
+        }
+        return null;
+    }
+
+    private Uri getImageUri(Context context) {
+        Resources resources = context.getResources();
+
+        Uri imageUri = new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .authority(resources.getResourcePackageName(R.drawable.android_cupcake))
+                .appendPath(resources.getResourceTypeName(R.drawable.android_cupcake))
+                .appendPath(resources.getResourceEntryName(R.drawable.android_cupcake))
+                .build();
+
+        return imageUri;
+    }
+
+    /**
+     * Getters
+     */
+    Uri getImageUri() {
+        return mImageUri;
+    }
+
+    private Data createInputDataForUri() {
+        Data.Builder builder = new Data.Builder();
+        if (mImageUri != null) {
+            builder.putString(KEY_IMAGE_URI, mImageUri.toString());
+        }
+        return builder.build();
+    }
+
+    LiveData<List<WorkInfo>> getOutputWorkInfo() { return mSavedWorkInfo; }
+
+    void setOutputUri(String outputImageUri) {
+        mOutputUri = uriOrNull(outputImageUri);
+    }
+
+    public Uri getOutputUri() {
+        return mOutputUri;
+    }
+
+    public void setOutputUri(Uri mOutputUri) {
+        this.mOutputUri = mOutputUri;
+    }
+
+    void cancelWork() {
+        mWorkManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME);
+    }
+}
---
+++ Week11/android-workmanager/app/src/main/java/com/example/background/workers/SaveImageToFileWorker.java
@@ -0,0 +1,67 @@
+package com.example.background.workers;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.background.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class SaveImageToFileWorker extends Worker {
+    public SaveImageToFileWorker(
+            @NonNull Context appContext,
+            @NonNull WorkerParameters workerParams) {
+        super(appContext, workerParams);
+    }
+
+    private static final String TAG = SaveImageToFileWorker.class.getSimpleName();
+
+    private static final String TITLE = "Blurred Image";
+    private static final SimpleDateFormat DATE_FORMATTER =
+            new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss z", Locale.getDefault());
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Context applicationContext = getApplicationContext();
+
+        // Makes a notification when the work starts and slows down the work so that it's easier to
+        // see each WorkRequest start, even on emulated devices
+        WorkerUtils.makeStatusNotification("Saving image", applicationContext);
+        WorkerUtils.sleep();
+
+        ContentResolver resolver = applicationContext.getContentResolver();
+        try {
+            String resourceUri = getInputData()
+                    .getString(Constants.KEY_IMAGE_URI);
+            Bitmap bitmap = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri)));
+            String outputUri = MediaStore.Images.Media.insertImage(
+                    resolver, bitmap, TITLE, DATE_FORMATTER.format(new Date()));
+            if (TextUtils.isEmpty(outputUri)) {
+                Log.e(TAG, "Writing to MediaStore failed");
+                return Result.failure();
+            }
+            Data outputData = new Data.Builder()
+                    .putString(Constants.KEY_IMAGE_URI, outputUri)
+                    .build();
+            return Result.success(outputData);
+        } catch (Exception exception) {
+            Log.e(TAG, "Unable to save image to Gallery", exception);
+            return Worker.Result.failure();
+        }
+    }
+}
